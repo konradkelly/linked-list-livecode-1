1c53593719ab960ff95c3ee463246e910b7b5b0c
konradkelly <konradky@gmail.com>
2026-01-20 15:21:24 -0800

    implement doubly linked-list and printBackwards method.

--- src/MyLL.java
+++ src/MyLL.java
@@ -1,32 +1,66 @@
-public class MyLL {
-    private class Node {
-        public char value;
-        public Node next;
+public class MyLL<T> {
+    private class Node<T> {
+        public T value;
+        public Node<T> next;
+        public Node<T> prev;
 
-        public Node(char value, Node next) {
+        public Node(T value, Node<T> next, Node<T> prev) {
             this.value = value;
             this.next = next;
+            this.prev = prev;
         }
     }
     
-    private Node head;
+    private Node<T> head;
+    private Node<T> tail;
 
     public MyLL() {
         head = null;
+        tail = null;
     }
     
-    public void printList(Node start) {
-        Node current = head;
+    public void printList(Node<T> start) {
+        Node<T> current = head;
         
         while(current != null) {
             System.out.println(current.value);
             current = current.next;
         }
     }
-    public boolean contains(char toFind) {
-        Node current = head;
+
+    public void addToBack(T toAdd) {
+        Node<T> newNode = new Node<T>(toAdd, null, null);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node<T> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+        newNode.prev = current;
+    }
+    
+    public void printBackwards() {
+        if (head == null) {
+            return;
+        }
+        Node<T> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        System.out.println(current.value);
+        while(current.prev != null) {
+            current = current.prev;
+            System.out.println(current.value);
+            }    
+        }
+
+    public boolean contains(T toFind) {
+        Node<T> current = head;
         while (current != null) {
-            if (current.value == toFind) {
+            if (current.value.equals(toFind)) {
                 return true;
             }
             current = current.next;
@@ -34,20 +68,20 @@ public class MyLL {
         return false;
     }
 
-    public char remove(char toRemove) {
+    public T remove(T toRemove) {
         if (head == null) {
-            return '\0';
+            return null;
         };
-        if (head.value == toRemove) {
+        if (head.value.equals(toRemove)) {
             head = head.next;
         }
-        Node current = head;
-        while (current != null) {
-            if (current.value == toRemove) {
+        Node<T> current = head;
+        while (current != null && current.next != null) {
+            if (current.next.value.equals(toRemove)) {
                 current.next = current.next.next;
             }
             current = current.next;
         }
-        return '\0';
+        return null;
     }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -1,21 +1,44 @@
 public class Practice {
     public static void main(String[] args) {
-        Node head = new Node('g', null);
-        Node ryan = new Node('s', null);
-        head.next = ryan;
-        ryan.next = new Node('t', null);
-        ryan.next.next = new Node('x', null);
+
+        // Node head = new Node('g', null);
+        // Node ryan = new Node('s', null);
+        // head.next = ryan;
+        // ryan.next = new Node('t', null);
+        // ryan.next.next = new Node('x', null);
         
-        head = head.next;
+        // head = head.next;
 
-        //printList(head);
+        // printList(head);
 
-        //Expect to return true
-        System.out.println(contains(head, 'x'));
+        // //Expect to return true
+        // System.out.println(contains(head, 'x'));
 
-        //Expect to return false
-        System.out.println(contains(head, 'g'));
-    }
+        // //Expect to return false
+        // System.out.println(contains(head, 'g'));
+
+        // remove(head, 't');
+        // remove(head, 'x');
+        // remove(head, 's');
+        MyLL<Character> list = new MyLL<>();
+
+        list.addToBack('x');
+        list.addToBack('m');
+        list.addToBack('v');
+        list.addToBack('q');
+        
+        list.printBackwards();
+
+        // list.printList(null);
+        //        printList(head);
+
+    MyLL<Integer> intList = new MyLL<>();
+            // intList.addToBack(5);
+            // intList.addToBack(99);
+            // intList.addToBack(-333333);
+            // intList.addToBack(66);
+            //intList.addToBack("Hello");
+        }
 
     public static void printList(Node start) {
         Node current = start;
@@ -25,6 +48,7 @@ public class Practice {
             current = current.next;
         }
     }
+
     public static boolean contains(Node start, char toFind) {
         Node current = start;
         while (current != null) {
@@ -37,12 +61,19 @@ public class Practice {
     }
 
     public static char remove(Node head, char toRemove) {
+        if (head == null) {
+            return '\0';
+        }
+        if (head.value == toRemove) {
+            head = head.next;
+        }
         Node current = head;
-        while (current != null) {
-            if (current.value == toRemove) {
+        while (current != null && current.next != null) {
+            if (current.next.value == toRemove) {
                 current.next = current.next.next;
+            } else {
+                current = current.next;
             }
-            current = current.next;
         }
         return '\0';
     }
